3ed20e40e224b08373c3f533ea66dd24a6b18a1a
IMP1 <huw_taylor@hotmail.com>
2020-01-14 15:58:19 +0000

    Update direction to reflect 0 = up

--- direction.rb
+++ direction.rb
@@ -3,11 +3,11 @@ module Direction
     def self.to_offset(direction)
         case direction
         when 0
-            return [0, 1]
+            return [0, -1]
         when 2
             return [-1, 0]
         when 4
-            return [0, -1]
+            return [0, 1]
         when 6
             return [1, 0]
         end
